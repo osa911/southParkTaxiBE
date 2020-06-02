280fe150d46239b152beeae54bb85314f578b2c6
Yevhenii <e.osadchuy@gmail.com>
2020-06-02 23:24:21 +0300

    Update netlify config

---
+++ functions/bundle/utils/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken')
+const { AuthenticationError } = require('apollo-server-express')
+const { NOT_AUTHENTICATED } = require('../constants/messages')
+
+function isAuthenticated(request) {
+  if (request) {
+    const Authorization = request.headers.authorization
+    if (Authorization) {
+      const token = Authorization.replace('Bearer ', '')
+      try {
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+        return { isOk: true, user }
+      } catch (e) {
+        throw new AuthenticationError(e.message)
+      }
+    }
+  }
+
+  return { isOk: false }
+}
+
+const checkIsAuth = (auth) => {
+  if (!auth.isOk) {
+    throw new AuthenticationError(NOT_AUTHENTICATED)
+  }
+}
+
+module.exports = {
+  isAuthenticated,
+  checkIsAuth,
+}
