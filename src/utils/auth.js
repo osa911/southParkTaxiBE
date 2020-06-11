f48ea0b9d7723d38990a01b0fec8e28676e65825
Yevhenii <e.osadchuy@gmail.com>
2020-06-11 23:13:55 +0300

    debug request

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,10 +3,13 @@ import { AuthenticationError } from 'apollo-server'
 import { NOT_AUTHENTICATED } from '../constants/messages'
 
 export const isAuthenticated = (request) => {
+  console.log('request> ', request)
   if (request) {
     const Authorization = request.headers.authorization
+    console.log('Authorization> ', Authorization)
     if (Authorization) {
       const token = Authorization.replace('Bearer ', '')
+      console.log('token> ', token)
       try {
         const user = jwt.verify(token, process.env.JWT_SECRET)
         return { isOk: true, user }
