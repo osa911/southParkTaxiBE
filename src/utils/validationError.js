a261afe3eb9bfd2a964047636fbac7f0c517a7d8
Yevhenii <e.osadchuy@gmail.com>
2020-08-10 22:37:57 +0300

    Update sending error message for validationError

--- src/utils/validationError.js
+++ src/utils/validationError.js
@@ -2,14 +2,20 @@ import { GraphQLError } from 'graphql'
 
 export class ValidationError extends GraphQLError {
   constructor(errors) {
-    super('The request is invalid.')
-    this.state = errors.reduce((result, error) => {
-      if (Object.prototype.hasOwnProperty.call(result, error.key)) {
-        result[error.key].push(error.message)
-      } else {
-        result[error.key] = [error.message]
+    super(errors)
+    if (Array.isArray(errors)) {
+      this.state = errors.reduce((result, error) => {
+        if (Object.prototype.hasOwnProperty.call(result, error.key)) {
+          result[error.key].push(error.message)
+        } else {
+          result[error.key] = [error.message]
+        }
+        return result
+      }, {})
+    } else if (typeof errors === 'string') {
+      this.state = {
+        message: errors,
       }
-      return result
-    }, {})
+    }
   }
 }
